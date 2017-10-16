ebb578d7ccf36509df338fed11a828b06242e1cc
BobVictorD <demeautis.victor@gmail.com>
2017-10-16 17:41:02 +0200

    ajout de l'url user/me afin de connaitre l'utilisateur connecté

--- src/main/java/Application/API/UserAPI.java
+++ src/main/java/Application/API/UserAPI.java
@@ -1,14 +1,30 @@
 package Application.API;
 
+import Application.Model.Activite;
 import Application.Model.User;
+import Application.Service.IActiviteService;
+import Application.Service.IUserService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.DefaultClaims;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserAPI extends APIController<User> {
+
+    private IUserService getService(){
+        return (IUserService) this.serv;
+    }
+
+    @RequestMapping("/me")
+    public User getByUser(HttpServletRequest request) throws ServletException {
+        String userLogin = ((DefaultClaims) request.getAttribute("claims")).getSubject();
+        return getService().findByLogin(userLogin).get(0);
+    }
 }
